e95b0f0a8777efc3723122667bbd873783c706a4
circlespainter <fabio@paralleluniverse.co>
2015-04-21 20:20:16 +0300

    Remove debugging printouts

--- src/jmh/java/ringbench/java/JavaThreadRingBenchmark.java
+++ src/jmh/java/ringbench/java/JavaThreadRingBenchmark.java
@@ -18,12 +18,14 @@ public class JavaThreadRingBenchmark extends AbstractRingBenchmark<JavaThreadRin
     @Override
     protected ThreadWorker[] setupWorkers(int[] sequences) {
         final ThreadWorker[] workers = new ThreadWorker[workerCount];
+
         for (int i = 0; i < workerCount; i++)
             workers[i] = new ThreadWorker(i, sequences);
-        for (int i = 0; i < workerCount; i++) {
+
+        // Set next worker pointers.
+        for (int i = 0; i < workerCount; i++)
             workers[i].next = workers[(i + 1) % workerCount];
-            workers[i].prev = workers[(i - 1 >= 0 ? i - 1 : workerCount - 1)];
-        }
+
         return workers;
     }
 
@@ -38,7 +40,6 @@ public class JavaThreadRingBenchmark extends AbstractRingBenchmark<JavaThreadRin
         protected volatile boolean waiting = true;
         protected int sequence = Integer.MAX_VALUE;
         protected volatile ThreadWorker next;
-        protected ThreadWorker prev;
 
         public ThreadWorker(final int id, final int[] sequences) {
             super(String.format("%s-%s-%d",
@@ -50,25 +51,16 @@ public class JavaThreadRingBenchmark extends AbstractRingBenchmark<JavaThreadRin
 
         @Override
         public void run() {
-            System.out.println("Thread " + Thread.currentThread().getName() +" starting");
             while (sequence > 0) {
-                System.out.println("Thread " + Thread.currentThread().getName() +" received " + sequence);
                 while (waiting) {
-                    System.out.println("Thread " + Thread.currentThread().getName() +" parking");
                     LockSupport.park();
                 }
-                System.out.println("Thread " + Thread.currentThread().getName() +" setting " + sequence + " and unparking next " + next.getName());
                 waiting = true;
                 next.sequence = sequence - 1;
                 next.waiting = false;
                 LockSupport.unpark(next);
             }
-            System.out.println("Thread " + Thread.currentThread().getName() +" finishing, exiting ring and unparking next " + next.getName());
-            prev.next = next;
-            next.waiting = false;
-            LockSupport.unpark(next);
             cdl.countDown();
-            System.out.println("Thread " + Thread.currentThread().getName() + " count is " + cdl.getCount());
             sequences[id] = sequence;
         }
     }
