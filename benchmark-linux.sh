e42af575a8dd2e5eafe9cd8b7e3a51d192e82b2e
circlespainter <fabio@paralleluniverse.co>
2015-04-21 20:09:59 +0300

    Linux-based benchmark with jmh perfasm

--- benchmark-linux.sh
+++ benchmark-linux.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-set -e
-
-projDir=$(dirname $(readlink -f "$0"))
+jfrOpts="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,dumponexit=true"
 
 findCpuList() {
     local cpuCount=$( \
@@ -13,15 +11,36 @@ findCpuList() {
     echo "0-$[$cpuCount-1]"
 }
 
+
+set -e
+
 [ -z "$workerCount" ] && workerCount=503
 [ -z "$ringSize" ] && ringSize=1000000
+[ -z "$quasarAgentLocation" ] && quasarAgentLocation=$HOME/.m2/repository/co/paralleluniverse/quasar-core/0.6.3-SNAPSHOT/quasar-core-0.6.3-SNAPSHOT-jdk8.jar
+[ -z "$bytemanAgentLocation" ] && bytemanAgentLocation="$HOME/.m2/repository/org/jboss/byteman/byteman/2.2.1/byteman-2.2.1.jar"
+[ -z "$warmupIters" ] && warmupIters=5
+[ -z "$iters" ] && iters=10
+[ -z "$stat" ] && stat=avg
+[ -z "$unit" ] && unit=ms
+[ -z "$forks" ] && forks=5
+[ -z "$benchRegexp" ] && benchRegexp=".*Benchmark.*"
 [ -z "$cpuList" ] && cpuList=$(findCpuList)
+[ "$enableJfr" != "true" ] && enableJfr=false && jfrOpts=""
 
 if [ "$1" = "-h" -o "$1" = "--help" ]; then
-    echo "Available parameters (with defaults):"
-    echo "    workerCount ($workerCount)"
-    echo "    ringSize    ($ringSize)"
-    echo "    cpuList     ($cpuList)"
+    echo "Available environment parameters (with defaults):"
+    echo "    workerCount             ($workerCount)"
+    echo "    ringSize                ($ringSize)"
+    echo "    quasarAgentLocation     ($quasarAgentLocation)"
+    echo "    bytemanAgentLocation    ($bytemanAgentLocation)"
+    echo "    warmupIters             ($warmupIters)"
+    echo "    iters                   ($iters)"
+    echo "    stat                    ($stat)"
+    echo "    unit                    ($unit)"
+    echo "    forks                   ($forks)"
+    echo "    benchRegexp             ($benchRegexp)"
+    echo "    cpuList                 ($cpuList)"
+    echo "    enableJfr               ($enableJfr)"
     exit 0
 fi
 
@@ -29,48 +48,17 @@ error() {
     echo "$@" >&2
 }
 
-requireFile() {
-    local name="$1"
-    local path="$2"
-    if [ -e "$path" ]
-    then echo "$path"
-    else
-        error "Could not locate $name."
-        error "Expected path: $path"
-        exit 1
-    fi
-}
+if [ ! -e "$quasarAgentLocation" ]; then 
+    error "Could not locate agent."
+    error "Expected path: $quasarAgentLocation"
+    exit 1
+fi
 
-pomFile=$(requireFile "POM file" "$projDir/pom.xml")
+# -Dorg.jboss.byteman.verbose
+# -Dco.paralleluniverse.fiber.verifyInstrumentation=true
+cmd="taskset -c $cpuList $JAVA_HOME/bin/java -Djmh.perfasm.hotThreshold=0.05 -Djmh.perfasm.tooBigThreshold=5000 -Djmh.perfasm.events=cycles,instructions,cache-misses -jar target/ring-bench.jar\
+ -jvmArgsAppend \"-Xbootclasspath/p:$bytemanAgentLocation $jfrOpts -Dorg.jboss.byteman.transform.all -DworkerCount=$workerCount -DringSize=$ringSize -javaagent:$quasarAgentLocation -javaagent:$bytemanAgentLocation=script:script.btm\"\
+ -wi $warmupIters -i $iters -bm $stat -tu $unit -f $forks -prof perfasm \"$benchRegexp\""
 
-findQuasarJar() {
-    local groupId="co.paralleluniverse"
-    local artifactId="quasar-core"
-    local version=$( \
-        grep -A 1 "<artifactId>$artifactId</artifactId>" "$pomFile" | \
-        tail -n 1 | \
-        sed -r 's/.*<version>(.*)<\/version>/\1/g')
-    local groupPath=$(echo "$groupId" | sed 's/\./\//g')
-    local repoDir="$HOME/.m2/repository"
-    requireFile \
-        "$groupId:$artifactId:$version JAR" \
-        "$repoDir/$groupPath/$artifactId/$version/$artifactId-$version-jdk8.jar"
-}
-
-artifactId=$( \
-    grep "artifactId" "$pomFile" | \
-    head -n 1 | \
-    sed -r 's/.*<artifactId>(.*)<\/artifactId>/\1/g')
-uberJar=$(requireFile \
-    "Uber JAR, run \"mvn install\" first" \
-    "$projDir/target/$artifactId.jar")
-quasarJar=$(findQuasarJar)
-
-cmd="taskset -c $cpuList \
-$JAVA_HOME/bin/java \
--server -XX:+TieredCompilation -XX:+AggressiveOpts \
--jar \"$uberJar\" \
--jvmArgsAppend \"-DworkerCount=$workerCount -DringSize=$ringSize -javaagent:$quasarJar\" \
--wi 5 -i 10 -bm avgt -tu ms -f 5 \".*RingBenchmark.*\""
 echo "$cmd"
 eval "$cmd"
